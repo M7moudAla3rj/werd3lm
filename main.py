1735a4da490c6eb1373c6006152dee72caa7c593
M7moudAla3rj <80818683+M7moudAla3rj@users.noreply.github.com>
2021-09-17 14:28:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+
+import schedule
+
+import time
+
+bot=telebot.TeleBot("1967626202:AAFykDZY-cKrU7UmOCkBxqTKAb97RxbTKCg")
+
+i=0
+
+imege="imeges/"+str(i)+".jpg"
+
+chat_id="1202602736"
+
+@bot.message_handler(commands=['start'])
+def start (message):
+  bot.reply_to(message , "welcomr")
+
+def imeges ():
+
+  bot.send_message(chat_id , "ورد اليوم من الدراسة")
+
+  img=open (imege , 'rb')
+
+  bot.send_photo(chat_id, img)
+
+def i ():
+
+  i = i + 1
+
+schedule.every().day.at("14:35:00").do(imeges)
+
+schedule.every().day.at("17:30:01").do(i)
+
+#while True:
+
+  #schedule.run_pending()
+bot.polling()
